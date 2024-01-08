b0ed2984fcaa96715df9e8e66def71736aa91099
Adrien LAROUSSE <adrien.larousse@telecomnancy.eu>
2024-01-08 10:27:05 +0100

    [feat] adding Account class and change credit type in database.db

--- src/main/java/eu/telecomnancy/directdealing/model/Admin.java
+++ src/main/java/eu/telecomnancy/directdealing/model/Admin.java
@@ -1,4 +1,7 @@
 package eu.telecomnancy.directdealing.model;
 
 public class Admin extends Account {
+    public Admin(String lastName, String firstName, String email) {
+        super(lastName, firstName, email);
+    }
 }
--- src/main/java/eu/telecomnancy/directdealing/model/User.java
+++ src/main/java/eu/telecomnancy/directdealing/model/User.java
@@ -5,4 +5,10 @@ public class User extends Account{
     float balance;
 
     boolean isSleeping;
+
+    public User(String lastName, String firstName, String email, float balance, boolean isSleeping) {
+        super(lastName, firstName, email);
+        this.balance = balance;
+        this.isSleeping = isSleeping;
+    }
 }
