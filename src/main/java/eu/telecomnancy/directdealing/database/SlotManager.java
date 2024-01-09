2a5e4c4f98cfd67300711634545124942a93dabc
Adrien LAROUSSE <adrien.larousse@telecomnancy.eu>
2024-01-09 11:45:26 +0100

    [feat] Slot Manager to get slot with slotid

---
+++ src/main/java/eu/telecomnancy/directdealing/database/SlotManager.java
@@ -0,0 +1,42 @@
+package eu.telecomnancy.directdealing.database;
+
+import eu.telecomnancy.directdealing.model.Admin;
+import eu.telecomnancy.directdealing.model.Slot;
+import eu.telecomnancy.directdealing.model.User;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.sql.*;
+
+public class SlotManager {
+    public Slot getSlotWithId(int idSlot) throws SQLException {
+        String query = "SELECT * FROM SLOT WHERE idSlot = ?";
+        ResultSet resultSet = null;
+
+        try (PreparedStatement preparedStatement = DatabaseAccess.connection.prepareStatement(query)) {
+            preparedStatement.setInt(1, idSlot);
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) { // Check if there are results
+                // recup des infos
+                int recupIdSlot = resultSet.getInt("idSlot");
+                Timestamp tempStartTime = resultSet.getTimestamp("startTime");
+                java.util.Date startTime = new java.util.Date(tempStartTime.getTime());
+                Timestamp tempEndTime = resultSet.getTimestamp("endTime");
+                java.util.Date endTime = new java.util.Date(tempEndTime.getTime());
+                int recurrence = resultSet.getInt("recurrence");
+
+                // Création du slot et renvoie
+                return new Slot(recupIdSlot, startTime, endTime, recurrence);
+
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        }
+        return null;
+
+    }
+
+}
