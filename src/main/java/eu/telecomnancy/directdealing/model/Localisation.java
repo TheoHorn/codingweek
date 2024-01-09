7d7e5fc5345b3ea5fb6878981eb4aaae67ac9bff
Adrien LAROUSSE <adrien.larousse@telecomnancy.eu>
2024-01-09 11:18:59 +0100

    [feat] model for localisation and slot

---
+++ src/main/java/eu/telecomnancy/directdealing/model/Localisation.java
@@ -0,0 +1,11 @@
+package eu.telecomnancy.directdealing.model;
+
+public class Localisation {
+    private double longitudeCoord;
+    private double latitudeCoord;
+
+    public Localisation(double longitudeCoord, double latitudeCoord){
+        this.longitudeCoord = longitudeCoord;
+        this.latitudeCoord = latitudeCoord;
+    }
+}
