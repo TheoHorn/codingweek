6f725700e0ac71e07d9116926dc900712707f55d
TheoHorn <82098718+TheoHorn@users.noreply.github.com>
2024-01-10 09:02:31 +0100

    [DOCS] offer documentation

--- src/main/java/eu/telecomnancy/directdealing/model/offer/Proposal.java
+++ src/main/java/eu/telecomnancy/directdealing/model/offer/Proposal.java
@@ -4,7 +4,17 @@ import eu.telecomnancy.directdealing.model.Slot;
 import eu.telecomnancy.directdealing.model.account.User;
 import eu.telecomnancy.directdealing.model.content.Content;
 
+/**
+ * Proposal class
+ */
 public class Proposal extends Offer {
+    /**
+     * Constructor of the proposal
+     * @param owner Owner of the proposal
+     * @param content Content of the proposal
+     * @param slot Slot of the proposal
+     * @param request Boolean to know if the proposal is a request or a proposal
+     */
     public Proposal(User owner, Content content, Slot slot, boolean request) {
 
         super(owner, content, slot, request);
--- src/main/java/eu/telecomnancy/directdealing/model/offer/Request.java
+++ src/main/java/eu/telecomnancy/directdealing/model/offer/Request.java
@@ -4,7 +4,17 @@ import eu.telecomnancy.directdealing.model.Slot;
 import eu.telecomnancy.directdealing.model.account.User;
 import eu.telecomnancy.directdealing.model.content.Content;
 
+/**
+ * Request class
+ */
 public class Request extends Offer {
+    /**
+     * Constructor of the request
+     * @param owner Owner of the request
+     * @param content Content of the request
+     * @param slot Slot of the request
+     * @param request Boolean to know if the request is a request or a proposal
+     */
     public Request(User owner, Content content, Slot slot,  boolean request){
         // Request is a subclass of Offer and inherits its constructor
         super(owner, content, slot, request);
