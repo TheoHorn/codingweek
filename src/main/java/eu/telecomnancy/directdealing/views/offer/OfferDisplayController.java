e4316a4d29f68eea1dc0b5ee1331acc619dd29b3
TheoHorn <82098718+TheoHorn@users.noreply.github.com>
2024-01-10 12:12:11 +0100

    [FEAT] add controller offer displaycontroller

---
+++ src/main/java/eu/telecomnancy/directdealing/views/offer/OfferDisplayController.java
@@ -0,0 +1,86 @@
+package eu.telecomnancy.directdealing.views.offer;
+
+import eu.telecomnancy.directdealing.model.Application;
+import eu.telecomnancy.directdealing.model.Observer;
+import eu.telecomnancy.directdealing.model.Slot;
+import eu.telecomnancy.directdealing.model.account.User;
+import eu.telecomnancy.directdealing.model.content.Content;
+import eu.telecomnancy.directdealing.model.offer.Offer;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class OfferDisplayController implements Observer {
+
+    private Application app;
+
+    @FXML
+    private Label title_label;
+
+    @FXML
+    private Label description_label;
+
+    @FXML
+    private Label price_label;
+
+    @FXML
+    private Label category_label;
+
+    @FXML
+    private Label disponibility_label;
+
+    @FXML
+    private Label owner_label;
+
+    @FXML
+    private Label type_label;
+
+    @FXML
+    private ImageView image_view;
+
+    @FXML
+    private Label location_label;
+
+
+    public OfferDisplayController() {
+        this.app = Application.getInstance();
+        this.app.addObserver(this);
+    }
+
+    @FXML
+    public void reservation() throws Exception {
+        // TODO
+    }
+
+    @FXML
+    public void contact() throws Exception {
+        // TODO
+    }
+
+    @FXML
+    public void proposeService() throws Exception {
+        // TODO
+    }
+    @Override
+    public void update() throws Exception {
+        Offer offer = this.app.getLastOffer();
+        Content content = offer.getContent();
+        Slot slot = offer.getSlot();
+        User owner = offer.getOwner();
+        this.title_label.setText(content.getTitle());
+        this.description_label.setText(content.getDescription());
+        this.price_label.setText(String.valueOf(content.getPrice()));
+        this.category_label.setText(content.getCategory());
+        this.disponibility_label.setText(slot.getDisponibility());
+        this.owner_label.setText(owner.getFirstName()+" "+owner.getLastName());
+        if (offer.isRequest()){
+            this.type_label.setText("Request");
+        } else {
+            this.type_label.setText("Proposal");
+        }
+        //this.image_view.setImage(new Image(content.getImage().impl_getUrl()));
+        //this.location_label.setText();
+
+    }
+}
