0aa2246b7e2036c88bcd35e88560d3610e81f42b
Adrien LAROUSSE <adrien.larousse@telecomnancy.eu>
2024-01-11 21:00:30 +0100

    [fix] list of myAnswers

--- src/main/java/eu/telecomnancy/directdealing/model/demande/DemandeManager.java
+++ src/main/java/eu/telecomnancy/directdealing/model/demande/DemandeManager.java
@@ -14,13 +14,17 @@ public class DemandeManager {
         String myMail = app.getCurrentUser().getEmail();
         List<Demande> myAnswers = new ArrayList<Demande>();
         List<Offer> myOffers = app.getOfferDAO().get(myMail);
-        System.out.println(myOffers);
+        System.out.println(myOffers.size());
         for (Demande demande : app.getDemandeDAO().get()){
             System.out.println(demande);
             int slotDemande = demande.getIdSlot();
             int offerDemande = app.getSlotDAO().get(slotDemande,false).getIdOffer();
-            if (!myOffers.contains(app.getOfferDAO().get(offerDemande))){
-                myAnswers.add(demande);
+            System.out.println(offerDemande);
+            for (Offer offer : myOffers){
+                if (offer.getIdOffer() == offerDemande){
+                    myAnswers.add(demande);
+                    break;
+                }
             }
         }
         return myAnswers;
