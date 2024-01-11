631b43c410db6d3668c2277edc74d38f9f343c8f
Adrien LAROUSSE <adrien.larousse@telecomnancy.eu>
2024-01-11 11:27:53 +0100

    [feat] fxml reponse view

---
+++ src/main/java/eu/telecomnancy/directdealing/views/reponse/ReponseViewController.java
@@ -0,0 +1,10 @@
+package eu.telecomnancy.directdealing.views.reponse;
+
+import eu.telecomnancy.directdealing.model.Observer;
+
+public class ReponseViewController implements Observer {
+    @Override
+    public void update() throws Exception {
+        // TODO
+    }
+}
