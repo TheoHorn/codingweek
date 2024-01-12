ac7b2c53397118b712c119b37f17428dae9ab0d7
Adrien LAROUSSE <adrien.larousse@telecomnancy.eu>
2024-01-12 11:03:37 +0100

    [feat] dispute model

--- src/main/java/eu/telecomnancy/directdealing/model/dispute/Dispute.java
+++ src/main/java/eu/telecomnancy/directdealing/model/dispute/Dispute.java
@@ -3,6 +3,22 @@ package eu.telecomnancy.directdealing.model.dispute;
 import eu.telecomnancy.directdealing.model.account.User;
 
 public class Dispute {
-    private User plaintiff;
-    private User Defendant;
+    private int idDispute;
+    private int idDemande;
+    private String content;
+    private String attacker;
+    private String defender;
+    public Dispute(int idDispute, int idDemande, String content, String attacker, String defender) {
+        this.idDispute = idDispute;
+        this.idDemande = idDemande;
+        this.content = content;
+        this.attacker = attacker;
+        this.defender = defender;
+    }
+    public Dispute(int idDemande, String content, String attacker, String defender) {
+        this.idDemande = idDemande;
+        this.content = content;
+        this.attacker = attacker;
+        this.defender = defender;
+    }
 }
